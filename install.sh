dc511db1b7c254241a04a0e022345493b265a35d
Volodymyr Savchenko <vladimir.savchenko@gmail.com>
2017-04-11 00:33:48 +0200

    install rmf

--- install.sh
+++ install.sh
@@ -2,6 +2,9 @@ tar xvzf osa10.2.tgz -C $HOME
 tar xvzf ic_tree_170410.tgz -C /data
 tar xvzf resources.tgz -C /data
 
+mkdir -pv /data/ic_tree_current/ic/ibis/rsp/
+cp -fv /data/resources/rmf_62bands.fits /data/ic_tree_current/ic/ibis/rsp/rmf_62bands.fits
+
 mkdir -pv /data/rep_base_prod/cat/hec/
 cp -fv /data/resources/gnrl_refr_cat_0040.fits /data/rep_base_prod/cat/hec/gnrl_refr_cat_0040.fits
 
