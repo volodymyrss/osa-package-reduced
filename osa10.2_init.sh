fdf68c47e90c375719cf1d25c62042daca3d10d1
Volodymyr Savchenko <vladimir.savchenko@gmail.com>
2017-04-10 17:02:31 +0200

    initial

---
+++ osa10.2_init.sh
@@ -0,0 +1,8 @@
+
+rm -rf $HOME/pfiles
+export ISDC_ENV=/osa10.2
+source $ISDC_ENV/bin/isdc_init_env.sh
+export INTEGRAL_DDCACHE_ROOT=/data/ddcache
+export CURRENT_IC=/local_data
+export INTEGRAL_DATA=/data/rep_base_prod
+export REP_BASE_PROD=/data/rep_base_prod
