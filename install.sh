41603f72fd17af0f2211135e67c5bb12c746015a
Volodymyr Savchenko <vladimir.savchenko@gmail.com>
2017-04-10 21:25:23 +0200

    adding refcat to resources in install

--- install.sh
+++ install.sh
@@ -1,3 +1,5 @@
 tar xvzf osa10.2.tgz -C $HOME
 tar xvzf ic_tree_170410.tgz -C /data
 tar xvzf resources.tgz -C /data
+mkdir -p /data/rep_base_prod/cat/hec/
+cp -fv /data/resources/gnrl_refr_cat_0040.fits /data/rep_base_prod/cat/hec/gnrl_refr_cat_0040.fits
