eb950bfc8869452095e2f5865062acd263d3de15
Volodymyr Savchenko <vladimir.savchenko@gmail.com>
2017-04-10 22:35:59 +0200

    aux adp ref included

--- install.sh
+++ install.sh
@@ -1,5 +1,9 @@
 tar xvzf osa10.2.tgz -C $HOME
 tar xvzf ic_tree_170410.tgz -C /data
 tar xvzf resources.tgz -C /data
-mkdir -p /data/rep_base_prod/cat/hec/
+
+mkdir -pv /data/rep_base_prod/cat/hec/
 cp -fv /data/resources/gnrl_refr_cat_0040.fits /data/rep_base_prod/cat/hec/gnrl_refr_cat_0040.fits
+
+mkdir -pv /data/rep_base_prod/aux/adp
+tar xvzf auxadpref.tgz -C /data/rep_base_prod/aux/adp
