278107b5d51a6ba07352cdaa166e67f7583237f3
Volodymyr Savchenko <vladimir.savchenko@gmail.com>
2017-04-10 20:42:47 +0200

    fixed pathes in init

--- osa10.2_init.sh
+++ osa10.2_init.sh
@@ -1,8 +1,8 @@
 
 rm -rf $HOME/pfiles
-export ISDC_ENV=/osa10.2
+export ISDC_ENV=/home/integral/osa10.2
 source $ISDC_ENV/bin/isdc_init_env.sh
 export INTEGRAL_DDCACHE_ROOT=/data/ddcache
-export CURRENT_IC=/local_data
+export CURRENT_IC=/data/ic_tree_current
 export INTEGRAL_DATA=/data/rep_base_prod
 export REP_BASE_PROD=/data/rep_base_prod
