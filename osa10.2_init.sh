4d2f4e3ab7627f54be40f7ebe4964a6ec3dc8af0
Volodymyr Savchenko <Volodymyr.Savchenko@unige.ch>
2018-03-19 12:36:57 +0100

    nrt support

--- osa10.2_init.sh
+++ osa10.2_init.sh
@@ -6,3 +6,5 @@ export INTEGRAL_DDCACHE_ROOT=/data/ddcache
 export CURRENT_IC=/data/ic_tree_current
 export INTEGRAL_DATA=/data/rep_base_prod
 export REP_BASE_PROD=/data/rep_base_prod
+export REP_BASE_PROD_CONS=/data/rep_base_prod
+export REP_BASE_PROD_NRT=/data/rep_base_prod_nrt
